4ebbdf63641edd4af38cc243da422e75e66a9ee0
maciej.tokarz@my-poi.pl <maciej.tokarz@my-poi.pl>
2018-01-09 23:39:29 +0100

    skręca, ale można lepiej...

--- machina.api/controllers/move.controller.js
+++ machina.api/controllers/move.controller.js
@@ -3,31 +3,42 @@ module.exports = function (pwm) {
   var motors = new motorsController(pwm)
 
   this.setMotors = function (data) {
+    const movement = { forward: 0, backward: 0, left: 0, right: 0 };
+
     const speedMin = 0;
     const speedMax = 1100;
-    const backwardMin = 1410;
-    const backwardMax = 1070;
+
     const forwardMin = 1550;
     const forwardMax = 1905;
+
+    const backwardMin = 1410;
+    const backwardMax = 1070;
+
+    const leftMin = 1410;
+    const leftMax = 1070;
+
+    const rightMin = 1550;
+    const rightMax = 1905;
 
     if (data.throttle >= 1410 && data.throttle <= 1550) {
       pwm.allChannelsOff();
       return;
     }
 
-    if (data.throttle > 1500) {
-      const forward = scaleBetween(data.throttle, forwardMin, forwardMax, speedMin, speedMax);
-      // console.log(forward)
-      motors.forwardLeft(forward);
-      motors.forwardRight(forward);
+    if (data.throttle > 1500) movement.forward = scaleBetween(data.throttle, forwardMin, forwardMax, speedMin, speedMax);
+    if (data.throttle < 1410) movement.backward = scaleBetween(data.throttle, backwardMin, backwardMax, speedMin, speedMax);
+    if (data.yaw < 1410) movement.left = scaleBetween(data.yaw, leftMin, leftMax, speedMin, speedMax);
+    if (data.yaw > 1500) movement.right = scaleBetween(data.yaw, rightMin, rightMax, speedMin, speedMax);
+
+    if (movement.forward > 0) {
+      motors.forwardLeft(movement.forward - movement.left);
+      motors.forwardRight(movement.forward - movement.right);
       return;
     }
 
-    if (data.throttle < 1410) {
-      const backward = scaleBetween(data.throttle, backwardMin, backwardMax, speedMin, speedMax);
-      // console.log(backward)
-      motors.backwardLeft(backward);
-      motors.backwardRight(backward);
+    if (movement.backward > 0) {
+      motors.backwardLeft(movement.backward - movement.left);
+      motors.backwardRight(movement.backward - movement.right);
       return;
     }
   }
@@ -36,62 +47,3 @@ module.exports = function (pwm) {
     return ((limitMax - limitMin) * (valueIn - baseMin) / (baseMax - baseMin)) + limitMin;
   }
 }
-
-
-
-// var axis0sign = axis0param > 0
-// var axis1sign = axis1param > 0
-
-// this.axis0 = Math.abs(axis0param)
-// this.axis1 = Math.abs(axis1param)
-
-// this.axis0 = microsecondsMin + ((this.axis0 - speedMin) * (microsecondsMax - microsecondsMin) / speedMax)
-// this.axis1 = microsecondsMin + ((this.axis1 - speedMin) * (microsecondsMax - microsecondsMin) / speedMax)
-
-// this.direction = axis1sign ? 'do tyłu' : 'naprzód'
-// if (this.axis1 < 160) this.direction = 'neutralny'
-
-// this.turn = axis0sign ? 'w prawo' : 'w lewo'
-// if (this.axis0 < 160) this.turn = 'neutralny'
-
-// // Wyłącz sterownik w strefie neutralnej
-// if (this.axis0 < 160 && this.axis1 < 160) pwm.allChannelsOff()
-// else {
-//   // Naprzód
-//   if (!axis1sign) {
-//     if (this.turn == 'w lewo') {
-//       if (this.axis0 > 180) {
-//         motors.forwardLeft(this.axis0)
-//         motors.forwardRight(this.axis1)
-//       } else motors.forward(this.axis1)
-//     }
-
-//     if (this.turn == 'w prawo') {
-//       if (this.axis0 > 180) {
-//         motors.forwardLeft(this.axis1)
-//         motors.forwardRight(this.axis0)
-//       } else motors.forward(this.axis1)
-//     }
-
-//     if (this.turn == 'neutralny') motors.forward(this.axis1)
-//   }
-
-//   // Do tyłu
-//   if (axis1sign) {
-//     if (this.turn == 'w lewo') {
-//       if (this.axis0 > 180) {
-//         motors.backwardsLeft(this.axis0)
-//         motors.backwardsRight(this.axis1)
-//       } else motors.backwards(this.axis1)
-//     }
-
-//     if (this.turn == 'w prawo') {
-//       if (this.axis0 > 180) {
-//         motors.backwardsLeft(this.axis1)
-//         motors.backwardsRight(this.axis0)
-//       } else motors.backwards(this.axis1)
-//     }
-
-//     if (this.turn == 'neutralny') motors.backwards(this.axis1)
-//   }
-// }
